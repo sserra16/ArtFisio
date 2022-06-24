ba8ab81902fcc78fe0bee647dba716a208ce6789
Samuel Serra <sserradev@gmail.com>
2022-06-23 21:59:24 -0300

    update

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { page: 'home' })
 });
 
-app.get('/painel', (req, res) => {
-    res.render('painel')
+app.get('/calendario', (req, res) => {
+    res.render('calendario', { page: "calendario" })
 });
 
 app.listen('3000', () => console.log('O server está rodando'));
